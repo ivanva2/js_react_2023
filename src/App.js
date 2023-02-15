cc3b7da4927642b0446701b64001296b67c7b472
Viktor Radkevich <viktor@MacBook-Pro-Viktor.local>
2023-02-15 14:19:00 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+// import './mycss.css'
+// import * as classstyle from './mycs.module.css'
+import React, { useEffect, useState } from 'react';
+import { Card } from 'antd'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [counter, setCounter] = useState()
+  const [users, setUsers] = useState([])
+
+
+  const increment = () => {
+    setCounter(counter+1)
+  }
+  const decrement = () => {
+    if (counter > 0) setCounter(counter-1)
+    
+  }
+
+  const getData = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(res => {
+        if (res && Array.isArray(res) && res.length > 0) {
+          setUsers(res)
+        } //typeof res == 'array'
+      })
+
+  }
+
+  const loadUsers = () => {
+    getData()
+  }
+
+  useEffect(()=>{
+    getData()
+  }, [])
+
+  
+
+  const styles = {
+    // 'border: 1 solid #000',
+    // color: 'red',
+    border: '1px solid #000',
+    padding: 10,
+    margin: 'auto',
+    borderRadius: 10,
+    marginBottom: 5,
+    // backgroudColor: '#333'
+  }
+  return(
+    <>
+      <h2>Users: <button type="" onClick={() => {loadUsers()}}>Load users</button></h2>
+      <div style={{margin: 50, display: 'flex', gap: 16}}>
+        {users.length > 0 &&
+          users.map(user => {
+            return <Card title={user.name} key={Math.random()} style={{width: 200}}><p  >{user.email}</p></Card>
+          })
+        }
+      </div>
+    </>
+  )
 }
 
 export default App;

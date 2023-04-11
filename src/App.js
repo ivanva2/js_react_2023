7a2a12ca01e6df1cf3405dddbc5cd92467243829
ivanva2 <vasiliev.ivan2003@yandex.ru>
2023-04-11 21:07:44 +0300

    z

--- src/App.js
+++ src/App.js
@@ -1,61 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
-// import './mycss.css'
-// import * as classstyle from './mycs.module.css'
 import React, { useEffect, useState } from 'react';
-import { Card } from 'antd'
+import { Card, Row } from 'antd'
 
+
+
+function changeBackground(color) {
+  document.body.style.background = color;
+}
+
+window.addEventListener("load",function() { changeBackground('#9d8ef6') });
 function App() {
-  const [counter, setCounter] = useState()
-  const [users, setUsers] = useState([])
-
-
-  const increment = () => {
-    setCounter(counter+1)
-  }
-  const decrement = () => {
-    if (counter > 0) setCounter(counter-1)
-    
-  }
+  const [users, setUsers] = useState([]);
+  const [userPosts, setPosts] = useState([]);
 
   const getData = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(res => {
-        if (res && Array.isArray(res) && res.length > 0) {
-          setUsers(res)
-        } //typeof res == 'array'
-      })
-
+    fetch('https://jsonplaceholder.typicode.com/users').then(Users => Users.json()).then(Users => 
+    {
+      if (Users && Array.isArray(Users) && Users.length > 0)
+      {
+        setUsers(Users);
+      }
+    })
+    
+    fetch('https://jsonplaceholder.typicode.com/posts').then(Posts => Posts.json()).then(Posts =>
+    {
+      if (Posts && Array.isArray(Posts) && Posts.length > 0)
+      {
+        setPosts(Posts);
+      }
+    })
   }
-
-  const loadUsers = () => {
-    getData()
-  }
-
-  useEffect(()=>{
-    getData()
-  }, [])
 
   
 
-  const styles = {
-    // 'border: 1 solid #000',
-    // color: 'red',
-    border: '1px solid #000',
-    padding: 10,
-    margin: 'auto',
-    borderRadius: 10,
-    marginBottom: 5,
-    // backgroudColor: '#333'
-  }
+  useEffect(()=>{
+    getData();
+  }, [])
+
+
   return(
     <>
-      <h2>Users: <button type="" onClick={() => {loadUsers()}}>Load users</button></h2>
-      <div style={{margin: 50, display: 'flex', gap: 16}}>
-        {users.length > 0 &&
-          users.map(user => {
-            return <Card title={user.name} key={Math.random()} style={{width: 200}}><p  >{user.email}</p></Card>
+      
+      
+      <div style={{margin: '0 auto', display: 'grid', gap: 10, width: 850 }}>
+        {users.length > 0 && users.map(user => {
+            return (
+            <Card class='AuthorLib' title={`Author: ${user.name}`} key={Math.random()} style = {{borderColor: '#aed2ea', backgroundColor: '#aed2ea'}}>
+              
+              <p>Email adress: <strong style = {{color: 'red'}}>{user.email}</strong></p>
+              <p>Phone nomber: <strong style = {{color: 'yellow'}}>{user.phone}</strong></p>
+              
+              <Row gutter={10}>  
+               {
+                userPosts.filter(post => post.userId === user.id).map(post =>{
+                  return(
+
+                      <Card title={post.title} style={{margin:10, borderColor: '#aed2ea'}} headStyle={{backgroundColor: '#3352ff', color: '#fff'}}>
+                        
+                        <p class = 'Paragraph'>{post.body}</p>
+
+                      </Card>
+
+                      ) 
+                
+                    } 
+                  )
+              }
+              </Row>
+
+            </Card>);
           })
         }
       </div>
